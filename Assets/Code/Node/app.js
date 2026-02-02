0d76a11aab978e4fa4c9bfc8aba421d7b08865b6
Maria Lindling <maria.neith.lindling@protonmail.com>
2026-02-02 10:06:28 +0100

    preliminary response server

--- Assets/Code/Node/app.js
+++ Assets/Code/Node/app.js
@@ -24,7 +24,10 @@ app.use( express.json() ) ;
 
 import { createConnection } from 'mysql2/promise';
 import { asyncMiddleware } from './util/asyncMiddleware.js';
-import { sessionContext } from './context/sessionc.js';
+import { sessionContext } from './context/ctx_session.js';
+import { inputContext } from './context/ctx_input.js';
+import { interactionNpcContext } from './context/ctx_interaction_npc.js';
+import { timeContext } from './context/ctx_time.js';
 
 let sql_connection = await createConnection({
   host: "localhost",
@@ -84,7 +87,6 @@ app.get( "/statistics/session", asyncMiddleware( async (request,response,next) =
   response.json( responseBody ) ;
 } ) ) ;
 
-
 app.get( "/statistics/session/:id", asyncMiddleware( async (request,response,next) => {
   /******/
   let responseBody = responseBodyGET ;
@@ -143,6 +145,241 @@ app.get( "/statistics/session/:id", asyncMiddleware( async (request,response,nex
   response.json( responseBody ) ;
 } ) ) ;
 
+app.get( "/statistics/input", asyncMiddleware( async (request,response,next) => {
+  /******/
+  let responseBody = responseBodyGET ;
+  const sql = "SELECT * FROM input ORDER BY session_id ASC" ;
+  /******/
+
+
+  /** Add Context */
+  responseBody.context = inputContext ;
+  /** End */
+
+
+  /** Maintain Custody Chain */
+  if (response.hasOwnProperty('custody_chain')) {
+    responseBody.custody_chain = response.custody_chain ;
+  }
+  responseBody.custody_chain.push( CUSTODIAN ) ;
+  /** End */
+
+
+  /** Query SQL Server */
+  /**
+    * TODO: SQL Query
+    */
+  /** End */
+  
+  response.json( responseBody ) ;
+} ) ) ;
+
+app.get( "/statistics/input/:id", asyncMiddleware( async (request,response,next) => {
+  /******/
+  let responseBody = responseBodyGET ;
+  const sql = "SELECT * FROM input WHERE session_id = ?" ;
+  let sql_params = [] ;
+  /******/
+
+
+  /** Add Context */
+  responseBody.context = sessionContext ;
+  /** End */
+  
+
+  /** Maintain Custody Chain */
+  if( request.hasOwnProperty('body') && request.body.hasOwnProperty('custody_chain') ) {
+    responseBody.custody_chain = request.body.custody_chain ;
+  }
+  responseBody.custody_chain.push( CUSTODIAN ) ;
+  /** End */
+  
+
+  /** Validate Data */
+  /**
+    * TODO: ensure that the fields contain valid datetime data
+    */
+  /** End */
+
+
+  /** Query SQL Server */
+  /**
+    * TODO: SQL Query
+    */
+  /** End */
+  
+  response.json( responseBody ) ;
+} ) ) ;
+
+app.get( "/statistics/interaction/npc", asyncMiddleware( async (request,response,next) => {
+  /******/
+  let responseBody = responseBodyGET ;
+  const sql = "SELECT * FROM interaction_npc ORDER BY session_id ASC" ;
+  /******/
+
+
+  /** Add Context */
+  responseBody.context = interactionNpcContext ;
+  /** End */
+
+
+  /** Maintain Custody Chain */
+  if (response.hasOwnProperty('custody_chain')) {
+    responseBody.custody_chain = response.custody_chain ;
+  }
+  responseBody.custody_chain.push( CUSTODIAN ) ;
+  /** End */
+
+
+  /** Query SQL Server */
+  /**
+    * TODO: SQL Query
+    */
+  /** End */
+  
+  response.json( responseBody ) ;
+} ) ) ;
+
+app.get( "/statistics/interaction/npc/:npc_id", asyncMiddleware( async (request,response,next) => {
+  /******/
+  let responseBody = responseBodyGET ;
+  const sql = "SELECT * FROM interaction_npc WHERE npc_id = ?" ;
+  let sql_params = [] ;
+  /******/
+
+
+  /** Add Context */
+  responseBody.context = interactionNpcContext ;
+  /** End */
+  
+
+  /** Maintain Custody Chain */
+  if( request.hasOwnProperty('body') && request.body.hasOwnProperty('custody_chain') ) {
+    responseBody.custody_chain = request.body.custody_chain ;
+  }
+  responseBody.custody_chain.push( CUSTODIAN ) ;
+  /** End */
+  
+
+  /** Validate Data */
+  /**
+    * TODO: ensure that the fields contain valid datetime data
+    */
+  /** End */
+
+
+  /** Query SQL Server */
+  /**
+    * TODO: SQL Query
+    */
+  /** End */
+  
+  response.json( responseBody ) ;
+} ) ) ;
+
+app.get( "/statistics/interaction/npc/:npc_id/:id", asyncMiddleware( async (request,response,next) => {
+  /******/
+  let responseBody = responseBodyGET ;
+  const sql = "SELECT * FROM interaction_npc WHERE npc_id = ? AND session_id = ?" ;
+  let sql_params = [] ;
+  /******/
+
+
+  /** Add Context */
+  responseBody.context = interactionNpcContext ;
+  /** End */
+  
+
+  /** Maintain Custody Chain */
+  if( request.hasOwnProperty('body') && request.body.hasOwnProperty('custody_chain') ) {
+    responseBody.custody_chain = request.body.custody_chain ;
+  }
+  responseBody.custody_chain.push( CUSTODIAN ) ;
+  /** End */
+  
+
+  /** Validate Data */
+  /**
+    * TODO: ensure that the fields contain valid datetime data
+    */
+  /** End */
+
+
+  /** Query SQL Server */
+  /**
+    * TODO: SQL Query
+    */
+  /** End */
+  
+  response.json( responseBody ) ;
+} ) ) ;
+
+app.get( "/statistics/time", asyncMiddleware( async (request,response,next) => {
+  /******/
+  let responseBody = responseBodyGET ;
+  const sql = "SELECT * FROM time ORDER BY session_id ASC" ;
+  /******/
+
+
+  /** Add Context */
+  responseBody.context = timeContext ;
+  /** End */
+
+
+  /** Maintain Custody Chain */
+  if (response.hasOwnProperty('custody_chain')) {
+    responseBody.custody_chain = response.custody_chain ;
+  }
+  responseBody.custody_chain.push( CUSTODIAN ) ;
+  /** End */
+
+
+  /** Query SQL Server */
+  /**
+    * TODO: SQL Query
+    */
+  /** End */
+  
+  response.json( responseBody ) ;
+} ) ) ;
+
+app.get( "/statistics/time/:id", asyncMiddleware( async (request,response,next) => {
+  /******/
+  let responseBody = responseBodyGET ;
+  const sql = "SELECT * FROM time WHERE session_id = ?" ;
+  let sql_params = [] ;
+  /******/
+
+
+  /** Add Context */
+  responseBody.context = timeContext ;
+  /** End */
+  
+
+  /** Maintain Custody Chain */
+  if( request.hasOwnProperty('body') && request.body.hasOwnProperty('custody_chain') ) {
+    responseBody.custody_chain = request.body.custody_chain ;
+  }
+  responseBody.custody_chain.push( CUSTODIAN ) ;
+  /** End */
+  
+
+  /** Validate Data */
+  /**
+    * TODO: ensure that the fields contain valid datetime data
+    */
+  /** End */
+
+
+  /** Query SQL Server */
+  /**
+    * TODO: SQL Query
+    */
+  /** End */
+  
+  response.json( responseBody ) ;
+} ) ) ;
+
 
 /*************************/
 /********* POST **********/
@@ -165,9 +402,9 @@ app.post( "/statistics/session", asyncMiddleware( async (request,response,next) 
 
   /** Maintain Custody Chain */
   if( request.hasOwnProperty('body') && request.body.hasOwnProperty('custody_chain') ) {
-    responseBodyGET.custody_chain = request.body.custody_chain ;
+    responseBody.custody_chain = request.body.custody_chain ;
   }
-  responseBodyGET.custody_chain.push( CUSTODIAN ) ;
+  responseBody.custody_chain.push( CUSTODIAN ) ;
   /** End */
   
 
@@ -197,6 +434,117 @@ app.post( "/statistics/session", asyncMiddleware( async (request,response,next) 
   response.json( responseBody ) ;
 } ) ) ;
 
+app.post( "/statistics/input", asyncMiddleware( async (request,response,next) => {
+  /******/
+  let responseBody = responseBodyPOST ;
+  const sql = "INSERT INTO input (session_id,times_buttons_clicked,distance_moved,etc) VALUES (?,?,?,?)" ;
+  let sql_params = [] ;
+  /******/
+
+
+  /** Add Context */
+  responseBody.context = inputContext ;
+  /** End */
+  
+
+  /** Maintain Custody Chain */
+  if( request.hasOwnProperty('body') && request.body.hasOwnProperty('custody_chain') ) {
+    responseBody.custody_chain = request.body.custody_chain ;
+  }
+  responseBody.custody_chain.push( CUSTODIAN ) ;
+  /** End */
+  
+
+  /** Validate Data */
+  /**
+    * TODO: ensure that the fields contain valid datetime data
+    */
+  /** End */
+
+
+  /** Query SQL Server */
+  /**
+    * TODO: SQL Query
+    */
+  /** End */
+  
+  response.json( responseBody ) ;
+} ) ) ;
+
+app.post( "/statistics/interaction/npc", asyncMiddleware( async (request,response,next) => {
+  /******/
+  let responseBody = responseBodyPOST ;
+  const sql = "INSERT INTO interaction_npc (session_id,npc_id,times_talked_to,lines_of_dialogue_skipped) VALUES (?,?,?,?)" ;
+  let sql_params = [] ;
+  /******/
+
+
+  /** Add Context */
+  responseBody.context = interactionNpcContext ;
+  /** End */
+  
+
+  /** Maintain Custody Chain */
+  if( request.hasOwnProperty('body') && request.body.hasOwnProperty('custody_chain') ) {
+    responseBody.custody_chain = request.body.custody_chain ;
+  }
+  responseBody.custody_chain.push( CUSTODIAN ) ;
+  /** End */
+  
+
+  /** Validate Data */
+  /**
+    * TODO: ensure that the fields contain valid datetime data
+    */
+  /** End */
+
+
+  /** Query SQL Server */
+  /**
+    * TODO: SQL Query
+    */
+  /** End */
+  
+  response.json( responseBody ) ;
+} ) ) ;
+
+app.post( "/statistics/time", asyncMiddleware( async (request,response,next) => {
+  /******/
+  let responseBody = responseBodyPOST ;
+  const sql = "INSERT INTO time (session_id,in_menus,in_exploration,in_dialogue) VALUES (?,?,?,?)" ;
+  let sql_params = [] ;
+  /******/
+
+
+  /** Add Context */
+  responseBody.context = timeContext ;
+  /** End */
+  
+
+  /** Maintain Custody Chain */
+  if( request.hasOwnProperty('body') && request.body.hasOwnProperty('custody_chain') ) {
+    responseBody.custody_chain = request.body.custody_chain ;
+  }
+  responseBody.custody_chain.push( CUSTODIAN ) ;
+  /** End */
+  
+
+  /** Validate Data */
+  /**
+    * TODO: ensure that the fields contain valid datetime data
+    */
+  /** End */
+
+
+  /** Query SQL Server */
+  /**
+    * TODO: SQL Query
+    */
+  /** End */
+  
+  response.json( responseBody ) ;
+} ) ) ;
+
 
 /*************************/
 /******* PUT/PATCH *******/
@@ -206,49 +554,150 @@ import { responseBodyPUT } from './format/putf.js';
 
 app.put( "/statistics/session", asyncMiddleware( async (request,response,next) => {
   /******/
-  //let responseBody = responseBodyPUT ;
-  //const sql = "UPDATE session SET ? = ? WHERE id = :id" ;
-  //let sql_params = [] ;
+  let responseBody = responseBodyPUT ;
+  const sql = "UPDATE session SET ? = ? WHERE id = :id" ;
+  let sql_params = [] ;
   /******/
 
 
   /** Add Context */
-  //responseBody.context = sessionContext ;
+  responseBody.context = sessionContext ;
   /** End */
   
 
   /** Maintain Custody Chain */
-  //if( request.hasOwnProperty('body') && request.body.hasOwnProperty('custody_chain') ) {
-  //  responseBodyGET.custody_chain = request.body.custody_chain ;
-  //}
-  //responseBodyGET.custody_chain.push( CUSTODIAN ) ;
+  if( request.hasOwnProperty('body') && request.body.hasOwnProperty('custody_chain') ) {
+    responseBody.custody_chain = request.body.custody_chain ;
+  }
+  responseBody.custody_chain.push( CUSTODIAN ) ;
   /** End */
   
 
   /** Validate Data */
-  //if( request.hasOwnProperty('body') && request.body.hasOwnProperty('started_at') && request.body.hasOwnProperty('ended_at') ) {
-    /**
-     * TODO: ensure that the fields contain valid datetime data
-     */
-  //  sql_params.push( request.body.started_at ) ;
-  //  sql_params.push( request.body.ended_at ) ;
-  //} else {
-  //  responseBody.ok = false ;
-  //  responseBody.error = `Invalid parameter.` ;
-  //  response.status(400) ;
-  //}
+  /**
+    * TODO: ensure that the fields contain valid datetime data
+    */
   /** End */
 
 
   /** Query SQL Server */
-  //let [ okPacket, _ ] = await sql_connection.execute( sql, sql_params ) ;
   /**
-    * TODO: error handling
+    * TODO: SQL Query
     */
-  //responseBody.insert_id = okPacket.insertId ;
   /** End */
   
-  response.json( /* responseBody */ ) ;
+  response.json( responseBody ) ;
+} ) ) ;
+
+app.put( "/statistics/input", asyncMiddleware( async (request,response,next) => {
+  /******/
+  let responseBody = responseBodyPUT ;
+  const sql = "UPDATE input SET ? = ? WHERE id = :id" ;
+  let sql_params = [] ;
+  /******/
+
+
+  /** Add Context */
+  responseBody.context = inputContext ;
+  /** End */
+  
+
+  /** Maintain Custody Chain */
+  if( request.hasOwnProperty('body') && request.body.hasOwnProperty('custody_chain') ) {
+    responseBody.custody_chain = request.body.custody_chain ;
+  }
+  responseBody.custody_chain.push( CUSTODIAN ) ;
+  /** End */
+  
+
+  /** Validate Data */
+  /**
+    * TODO: ensure that the fields contain valid datetime data
+    */
+  /** End */
+
+
+  /** Query SQL Server */
+  /**
+    * TODO: SQL Query
+    */
+  /** End */
+  
+  response.json( responseBody ) ;
+} ) ) ;
+
+app.put( "/statistics/interaction/npc", asyncMiddleware( async (request,response,next) => {
+  /******/
+  let responseBody = responseBodyPUT ;
+  const sql = "UPDATE interaction_npc SET ? = ? WHERE id = :id" ;
+  let sql_params = [] ;
+  /******/
+
+
+  /** Add Context */
+  responseBody.context = interactionNpcContext ;
+  /** End */
+  
+
+  /** Maintain Custody Chain */
+  if( request.hasOwnProperty('body') && request.body.hasOwnProperty('custody_chain') ) {
+    responseBody.custody_chain = request.body.custody_chain ;
+  }
+  responseBody.custody_chain.push( CUSTODIAN ) ;
+  /** End */
+  
+
+  /** Validate Data */
+  /**
+    * TODO: ensure that the fields contain valid datetime data
+    */
+  /** End */
+
+
+  /** Query SQL Server */
+  /**
+    * TODO: SQL Query
+    */
+  /** End */
+  
+  response.json( responseBody ) ;
+} ) ) ;
+
+app.put( "/statistics/time", asyncMiddleware( async (request,response,next) => {
+  /******/
+  let responseBody = responseBodyPUT ;
+  const sql = "UPDATE time SET ? = ? WHERE id = :id" ;
+  let sql_params = [] ;
+  /******/
+
+
+  /** Add Context */
+  responseBody.context = timeContext ;
+  /** End */
+  
+
+  /** Maintain Custody Chain */
+  if( request.hasOwnProperty('body') && request.body.hasOwnProperty('custody_chain') ) {
+    responseBody.custody_chain = request.body.custody_chain ;
+  }
+  responseBody.custody_chain.push( CUSTODIAN ) ;
+  /** End */
+  
+
+  /** Validate Data */
+  /**
+    * TODO: ensure that the fields contain valid datetime data
+    */
+  /** End */
+
+
+  /** Query SQL Server */
+  /**
+    * TODO: SQL Query
+    */
+  /** End */
+  
+  response.json( responseBody ) ;
 } ) ) ;
 
 
@@ -280,37 +729,123 @@ app.delete( "/statistics/session", asyncMiddleware( async (request,response,next
 
   /** Maintain Custody Chain */
   if( request.hasOwnProperty('body') && request.body.hasOwnProperty('custody_chain') ) {
-    responseBodyGET.custody_chain = request.body.custody_chain ;
+    responseBody.custody_chain = request.body.custody_chain ;
   }
-  responseBodyGET.custody_chain.push( CUSTODIAN ) ;
-  /** End */
-  
-
-  /** Validate Data */
-  if( request.hasOwnProperty('body') && request.body.hasOwnProperty('started_at') && request.body.hasOwnProperty('ended_at') ) {
-    /**
-     * TODO: ensure that the fields contain valid datetime data
-     */
-    sql_params.push( request.body.started_at ) ;
-    sql_params.push( request.body.ended_at ) ;
-  } else {
-    responseBody.ok = false ;
-    responseBody.error = `Invalid parameter.` ;
-    response.status(400) ;
-  }
+  responseBody.custody_chain.push( CUSTODIAN ) ;
   /** End */
 
 
   /** Query SQL Server */
-  let [ okPacket, _ ] = await sql_connection.execute( sql, sql_params ) ;
   /**
-    * TODO: error handling
+    * TODO: SQL Query
     */
-  responseBody.insert_id = okPacket.insertId ;
   /** End */
   
   response.json( responseBody ) ;
 } ) ) ;
+
+app.delete( "/statistics/input", asyncMiddleware( async (request,response,next) => {
+  /******/
+  let responseBody = responseBodyDELETE ;
+  const sql = "DELETE FROM input WHERE id = :id" ;
+  let sql_params = [] ;
+  let deletions = {
+    "/statistics/input": 0,
+    "_comment": "location and number of deletions"
+  } ;
+  /******/
+
+
+  /** Add Context */
+  responseBody.context = inputContext ;
+  /** End */
+  
+
+  /** Maintain Custody Chain */
+  if( request.hasOwnProperty('body') && request.body.hasOwnProperty('custody_chain') ) {
+    responseBody.custody_chain = request.body.custody_chain ;
+  }
+  responseBody.custody_chain.push( CUSTODIAN ) ;
+  /** End */
+
+
+  /** Query SQL Server */
+  /**
+    * TODO: SQL Query
+    */
+  /** End */
+  
+  response.json( responseBody ) ;
+} ) ) ;
+
+app.delete( "/statistics/interaction/npc", asyncMiddleware( async (request,response,next) => {
+  /******/
+  let responseBody = responseBodyDELETE ;
+  const sql = "DELETE FROM interaction_npc WHERE id = :id" ;
+  let sql_params = [] ;
+  let deletions = {
+    "/statistics/interaction/npc": 0,
+    "_comment": "location and number of deletions"
+  } ;
+  /******/
+
+
+  /** Add Context */
+  responseBody.context = interactionNpcContext ;
+  /** End */
+  
+
+  /** Maintain Custody Chain */
+  if( request.hasOwnProperty('body') && request.body.hasOwnProperty('custody_chain') ) {
+    responseBody.custody_chain = request.body.custody_chain ;
+  }
+  responseBody.custody_chain.push( CUSTODIAN ) ;
+  /** End */
+
+
+  /** Query SQL Server */
+  /**
+    * TODO: SQL Query
+    */
+  /** End */
+  
+  response.json( responseBody ) ;
+} ) ) ;
+
+app.delete( "/statistics/time", asyncMiddleware( async (request,response,next) => {
+  /******/
+  let responseBody = responseBodyDELETE ;
+  const sql = "DELETE FROM time WHERE id = :id" ;
+  let sql_params = [] ;
+  let deletions = {
+    "/statistics/time": 0,
+    "_comment": "location and number of deletions"
+  } ;
+  /******/
+
+
+  /** Add Context */
+  responseBody.context = timeContext ;
+  /** End */
+  
+
+  /** Maintain Custody Chain */
+  if( request.hasOwnProperty('body') && request.body.hasOwnProperty('custody_chain') ) {
+    responseBody.custody_chain = request.body.custody_chain ;
+  }
+  responseBody.custody_chain.push( CUSTODIAN ) ;
+  /** End */
+
+
+  /** Query SQL Server */
+  /**
+    * TODO: SQL Query
+    */
+  /** End */
+  
+  response.json( responseBody ) ;
+} ) ) ;
+
 
 
 /*************************/
