550a8ae7fb2e92bf0f344fd1cdb8395d4926056e
Maria Lindling <maria.neith.lindling@protonmail.com>
2026-02-05 15:45:36 +0100

    add DebugMessage TryInvoke overloads and option to force the message

--- Assets/Code/DebugMessage.cs
+++ Assets/Code/DebugMessage.cs
@@ -8,12 +8,25 @@ public class DebugMessage
   [SerializeField] public bool isActive ;
   [SerializeField] public string message ;
 
-  public bool TryInvoke()
+  public bool TryInvoke(bool force = false)
   {
 #if UNITY_EDITOR
-  if( isActive )
-    Debug.Log( message ) ;
+    if( isActive )
+      Debug.Log( message ) ;
 #endif
+    if( !isActive && force )
+      Debug.Log( message ) ;
+    return isActive ;
+  }
+
+  public bool TryInvoke(string value, bool force = false)
+  {
+#if UNITY_EDITOR
+    if( isActive )
+      Debug.Log( $"{message}::{value}" ) ;
+#endif
+    if( !isActive && force )
+      Debug.Log( $"{message}::{value}" ) ;
     return isActive ;
   }
 }
